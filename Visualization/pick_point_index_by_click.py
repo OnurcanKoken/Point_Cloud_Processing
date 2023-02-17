54ff9b0f0afa76ac2cee52d924bbea704b4324eb
köken <onur-koken@hotmail.com>
2023-02-17 10:01:14 +0100

    VisualizerWithEditing

---
+++ Visualization/pick_point_index_by_click.py
@@ -0,0 +1,24 @@
+import numpy as np
+import open3d as o3d
+
+pcd1 = o3d.io.read_point_cloud("data\DemoICPPointClouds\cloud1.pcd")
+
+origin = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.5)
+
+vis = o3d.visualization.VisualizerWithEditing()
+vis.create_window()
+vis.add_geometry(pcd1)
+vis.run()
+
+vis.destroy_window()
+print(vis.get_picked_points())
+
+# for example:
+#pcd_point_index = [68461, 34211, 32476, 109543, 82416, 77840, 72361, 63780, 27196, 31052, 92, 2604, 79505, 91671, 113529, 59625]
+#cloud_points = np.asarray(pcd1.points)
+#for i in range(len(pcd_point_index)):
+#    # print each point corresponding to each index
+#    print(cloud_points[pcd_point_index[i]])
+
+# Reference: https://stackoverflow.com/questions/56183844/how-can-we-pick-3d-points-from-point-cloud-data-from-a-pcd-file-using-python
+# access date: 17.02.2023
